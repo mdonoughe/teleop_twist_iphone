46b3423e1c6377718614a33d336b49e96e4629c3
Matthew Donoughe <mdonoughe@gmail.com>
2011-03-14 18:57:43 -0400

    fix and turn into a proper node

--- bin/teleop_twist_iphone.py
+++ bin/teleop_twist_iphone.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('iphone_teleop')
+import roslib; roslib.load_manifest('teleop_twist_iphone')
 import rospy
 
 from geometry_msgs.msg import Twist
@@ -8,17 +8,23 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
+from threading import Thread
+
 MAX_SPEED = 0.5
 MAX_ROT = 0.5
 pub = None
 
 def go(angular, linear):
-    twist = Twist()
-    twist.linear.x = MAX_SPEED * linear
-    twist.linear.y = twist.linear.z = 0
-    twist.angular.x = twist.angluar.y = 0
-    twist.angular.z = MAX_ROT * angular
-    pub.publish(twist)
+    try:
+        print(angular, linear)
+        twist = Twist()
+        twist.linear.x = MAX_SPEED * linear
+        twist.linear.y = twist.linear.z = 0
+        twist.angular.x = twist.angular.y = 0
+        twist.angular.z = MAX_ROT * angular
+        pub.publish(twist)
+    except Exception as e:
+        print(e)
 
 def stop():
     go(0, 0)
@@ -26,31 +32,36 @@ def stop():
 class PageHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Content-Type', 'text/html')
-        with open('page.html', 'r') as f:
+        with open(roslib.packages.resource_file('teleop_twist_iphone', 'data', 'page.html'), 'r') as f:
             self.write(f.read())
 
 class CrossHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Content-Type', 'image/png')
-        with open('cross.png', 'r') as f:
+        with open(roslib.packages.resource_file('teleop_twist_iphone', 'data', 'cross.png'), 'r') as f:
             self.write(f.read())
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         # convert and clamp input
-        x, y = [max(-1, max(float(a), 1)) for a in message.split(',')]
+        x, y = [max(-1, min(float(a), 1)) for a in message.split(',')]
         go(x, y)
 
     def on_close(self):
         stop()
 
+def wait_for_exit():
+    rospy.spin()
+    tornado.ioloop.IOLoop.instance().stop()
+
 def main():
     global pub
     pub = rospy.Publisher('cmd_vel', Twist)
-    rospy.init_node('iphone_teleop')
+    rospy.init_node('teleop_twist_iphone')
 
     server = tornado.web.Application([('/', PageHandler), ('/cross.png', CrossHandler), ('/socket', SocketHandler)])
     server.listen(3000)
+    Thread(target = wait_for_exit).start()
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
